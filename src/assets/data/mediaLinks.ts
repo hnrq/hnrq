3c6c21c9693e57f46de422e359dcbb913d1bcbc3
Henrique Ramos <rickeight@hotmail.com>
2021-03-08 13:34:43 -0300

    Update mediaLinks.ts

--- src/assets/data/mediaLinks.ts
+++ src/assets/data/mediaLinks.ts
@@ -3,7 +3,7 @@ export default {
   whatsApp: 'https://wa.me/5531986738183',
   twitter: 'https://twitter.com/alberoneramos',
   twitch: 'https://twitch.com/alberoneramos',
-  linkedin: 'https://www.linkedin.com/in/henrique-ramos-83406410b/',
+  linkedin: 'www.linkedin.com/in/hnriq',
   deviantArt: 'https://deviantart.com/alberoneramos',
   devTo: 'https://dev.to/hnrq',
   instagram: 'https://instagram.com/alberoneramos'
