bcb46ce39a3d7fa710b61a4631e0ab7ced663bcb
Henrique Ramos <henriquealberone@outlook.com>
2022-09-30 15:33:34 -0300

    chore: move header to +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Header from '@src/layout/Header.svelte';
+</script>
+
+<svelte:head>
+	<script>
+		if ('theme' in localStorage) document.documentElement.classList.add(localStorage.theme);
+		else
+			document.documentElement.classList.add(
+				window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+			);
+	</script>
+
+	<style>
+		body {
+			font-family: var(--body-font);
+			font-size: 18px;
+			background: var(--background-color);
+			color: var(--text-color);
+			box-sizing: border-box;
+		}
+
+		@media only screen and (max-width: 600px) {
+			html {
+				font-size: 14px;
+			}
+		}
+
+		body {
+			margin: 0;
+		}
+	</style>
+</svelte:head>
+
+<Header />
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,20 @@
 <script>
-	import Header from '@src/layout/Header.svelte';
-
+	import '@src/theme/index.scss';
+	import About from './sections/About.svelte';
 	import Home from './sections/Home.svelte';
 </script>
 
-<svelte:head>
-	<style>
-		html {
-			font-family: var(--body-font);
-			font-size: 18px;
-			background: var(--background-color);
-			color: var(--text-color);
-			box-sizing: border-box;
-		}
-
-		@media only screen and (max-width: 600px) {
-			html {
-				font-size: 14px;
-			}
-		}
-
-		body {
-			margin: 0;
-		}
-	</style>
-</svelte:head>
-
-<Header />
-<Home />
+<div class="index-page">
+	<Home />
+	<About />
+</div>
 
 <style lang="scss">
-	@use '@src/theme';
+	@use '@src/theme/utils';
+
+	.index-page {
+		display: flex;
+		flex-direction: column;
+		row-gap: utils.spacing(12);
+	}
 </style>
