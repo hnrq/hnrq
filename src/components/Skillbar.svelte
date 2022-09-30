960976dbea2cd58c7565f580b2d8e6126f152360
Henrique Ramos <henriquealberone@outlook.com>
2022-09-30 00:33:43 -0300

    feat: add Skillbar component

---
+++ src/components/Skillbar.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	export let label: string;
+	export let value: number;
+</script>
+
+<div class="skillbar">
+	<div class="skillbar__info">
+		<span class="skillbar__label">{label}</span>
+		<span class="skillbar__value">{value}/10</span>
+	</div>
+
+	<div class="skillbar__bar-container">
+		<div class="skillbar__bar" data-testid="skillbar__bar" style={`width: ${value * 10}%`} />
+	</div>
+</div>
+
+<style lang="scss">
+	@use '@src/theme/utils';
+	$border-width: 2px;
+
+	.skillbar {
+		display: flex;
+		box-sizing: border-box;
+		font-family: var(--body-font);
+
+		&__info {
+			display: flex;
+			justify-content: space-between;
+		}
+
+		&__bar-container {
+			border: $border-width solid var(--primary-color);
+			position: relative;
+			box-sizing: border-box;
+			height: utils.spacing(5);
+			width: 100%;
+		}
+
+		&__bar {
+			height: utils.spacing(5);
+			position: absolute;
+			left: -#{calc($border-width / 2)};
+			left: -#{calc($border-width)};
+			background-color: var(--primary-color);
+		}
+	}
+</style>
---
+++ tests/components/Skillbar.test.ts
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker';
+import Skillbar from '@src/components/Skillbar.svelte';
+import { render } from '@testing-library/svelte';
+
+describe('<Skillbar />', () => {
+	it('renders a label', () => {
+		const skillbar = { label: faker.word.noun(), value: faker.datatype.float(10) };
+		const { getByText } = render(Skillbar, skillbar);
+
+		expect(getByText(skillbar.label)).toBeInTheDocument();
+	});
+
+	it('receives a value from 0 to 10', () => {
+		const skillbar = { label: faker.word.noun(), value: faker.datatype.float(10) };
+		const { getByText } = render(Skillbar, skillbar);
+
+		expect(getByText(`${skillbar.value}/10`)).toBeInTheDocument();
+	});
+
+	it('renders the skill bar with the width based on the value', () => {
+		const skillbar = { label: faker.word.noun(), value: faker.datatype.float(10) };
+		const { getByTestId } = render(Skillbar, skillbar);
+
+		expect(getByTestId('skillbar__bar').style.width).toBe(`${skillbar.value * 10}%`);
+	});
+});
