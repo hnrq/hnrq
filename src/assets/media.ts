088a077a61fbf8bd01fbff3b2ab7d82fcfda054e
Henrique Ramos <henriquealberone@outlook.com>
2022-09-30 15:34:36 -0300

    feat: add media and skills

---
+++ src/assets/media.ts
@@ -0,0 +1,9 @@
+export default [
+	{ label: 'GitHub', url: 'https://github.com/hnrq' },
+	{ label: 'Twitter', url: 'https://twitter.com/alberoneramos' },
+	{ label: 'Twitch', url: 'https://twitch.com/alberoneramos' },
+	{ label: 'LinkedIn', url: 'www.linkedin.com/in/hnriq' },
+	{ label: 'DeviantArt', url: 'https://deviantart.com/alberoneramos' },
+	{ label: 'Dev.to', url: 'https://dev.to/hnrq' },
+	{ label: 'Instagram', url: 'https://instagram.com/hnrq.zip' }
+];
---
+++ src/assets/skills.ts
@@ -0,0 +1,6 @@
+export default [
+	{ label: 'Javascript', value: 7.5 },
+	{ label: 'React.js', value: 7.2 },
+	{ label: 'CSS', value: 6.6 },
+	{ label: 'Flutter', value: 5.5 }
+];
