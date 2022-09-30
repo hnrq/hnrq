917a6d88c1b71bd1181b95607b900e8db0beef02
Henrique Ramos <henriquealberone@outlook.com>
2022-09-30 00:36:44 -0300

    feat: create index page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Header from '@src/layout/Header.svelte';
+
+	import Home from './sections/Home.svelte';
+</script>
+
+<svelte:head>
+	<style>
+		html {
+			font-family: var(--body-font);
+			font-size: 18px;
+			background: var(--background-color);
+			color: var(--text-color);
+			box-sizing: border-box;
+		}
+
+		@media only screen and (max-width: 600px) {
+			html {
+				font-size: 14px;
+			}
+		}
+
+		body {
+			margin: 0;
+		}
+	</style>
+</svelte:head>
+
+<Header />
+<Home />
+
+<style lang="scss">
+	@use '@src/theme';
+</style>
