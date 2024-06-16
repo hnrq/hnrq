6d8792312d4ae5d24e15c1a2f473bf850d1a43f6
Henrique Ramos <henriquealberone@outlook.com>
2024-06-15 22:12:48 -0300

    chore: update vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import gltf from 'vite-plugin-gltf';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import { dedup, draco, prune } from '@gltf-transform/functions';
+
+export default defineConfig({
+  plugins: [gltf({ transforms: [prune(), dedup(), draco()] }), tsconfigPaths()],
+});
