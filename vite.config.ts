16724c1231aab970a1c483679951ebd3caef706b
Henrique Ramos <henriquealberone@outlook.com>
2025-06-25 15:46:33 -0300

    chore: add Threlte

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
+	plugins: [
+		{ ...threeMinifier(), enforce: 'pre' }, // <=== Add plugin here
+		sveltekit(),
+		devtoolsJson()
+	]
 });
